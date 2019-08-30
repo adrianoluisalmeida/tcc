6f97c126d9fbaceb4f2beb4fc4990c2c3cbaa4f1
Verdclub <contato@verdclub.com>
2019-08-29 22:35:44 -0300

    for para percorrer os csvs desde 1986 at 2018

--- import_csv/index.php
+++ import_csv/index.php
@@ -1,21 +1,30 @@
 <?php
 
-$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-$bulk = new MongoDB\Driver\BulkWrite;
 
-echo "Connection to database successfully";
 
-$arquivocsv = "2018.csv";
 
-$handle = file($arquivocsv);
 
-for ($i=1;$i<count($handle);$i++){
+
+for($j = 1986; $j <= 2018; $j++){
+
+    $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+    $bulk = new MongoDB\Driver\BulkWrite;
+    echo "Connection to database successfully";
+
+    $arquivocsv = $j . ".csv";
+    echo $j . "\n";
+
+    $handle = file($arquivocsv);
+
+    for ($i=1;$i<count($handle);$i++){
+        
+        $valori = explode(",", $handle[$i]);
+        $date = date('Y-m-d', strtotime(trim($valori[1])));
+        $code = trim($valori[3]);
+        $price_last =  trim($valori[10]);
+
+        $bulk->insert(['date' => $date, 'code' => $code, 'price' => $price_last]);
+    }
+    $manager->executeBulkWrite('yahoo.import', $bulk);
     
-    $valori = explode(",", $handle[$i]);
-    $date = date('Y-m-d', strtotime(trim($valori[1])));
-    $code = trim($valori[3]);
-    $price_last =  trim($valori[10]);
-
-    $bulk->insert(['date' => $date, 'code' => $code, 'price' => $price_last]);
 }
-$manager->executeBulkWrite('yahoo.import', $bulk);
